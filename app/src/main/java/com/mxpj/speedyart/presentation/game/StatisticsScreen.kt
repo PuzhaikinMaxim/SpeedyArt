6fa93c4405451ae3568cb6580bcbc7faef242aa3
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2023-08-08 23:10:02 +0300

    Добавлен экран со статистикой

--- app/src/main/java/com/mxpj/speedyart/presentation/game/StatisticsScreen.kt
+++ app/src/main/java/com/mxpj/speedyart/presentation/game/StatisticsScreen.kt
@@ -3,22 +3,27 @@ package com.mxpj.speedyart.presentation.game
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.progressSemantics
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.ProgressIndicatorDefaults
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.LightGray
-import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.mxpj.speedyart.R
+import com.mxpj.speedyart.ui.theme.ProgressYellow
 
 @Preview
 @Composable
@@ -28,18 +33,20 @@ fun StatisticsScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
         ) {
-            generalProgress()
+            GeneralProgress()
+            Spacer(modifier = Modifier.height(20.dp))
+            ConcreteProgresses()
         }
     }
 }
 
 @Preview
 @Composable
-fun generalProgress() {
-
+fun GeneralProgress() {
+    val strokeWidth = 20.dp
     Spacer(modifier = Modifier.height(30.dp))
     val stroke = with(LocalDensity.current) {
-        Stroke(width = 15.dp.toPx(), cap = StrokeCap.Butt)
+        Stroke(width = strokeWidth.toPx(), cap = StrokeCap.Butt)
     }
 
     Box() {
@@ -52,14 +59,71 @@ fun generalProgress() {
                 color = LightGray, style = stroke
             )
         }
+        Text(
+            text = "70%",
+            modifier = Modifier.align(Alignment.Center),
+            fontSize = 20.sp
+        )
         CircularProgressIndicator(
             progress = 0.8f,
-            strokeWidth = 15.dp,
-            color = Color.Green,
+            strokeWidth = strokeWidth,
+            color = ProgressYellow,
             modifier = Modifier
                 .fillMaxWidth(0.6f)
                 .aspectRatio(1f)
         )
     }
+}
 
+@Preview
+@Composable
+fun ConcreteProgresses() {
+    Column(modifier = Modifier.fillMaxWidth(0.8f)/*.background(Gray)*/) {
+        ConcreteProgress(
+            text = stringResource(R.string.amount_pictures_completed, 5, 10),
+            progress = 0.5f,
+            progressInPercents = 50
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        ConcreteProgress(
+            text = stringResource(R.string.amount_stars, 5, 10),
+            progress = 0.5f,
+            progressInPercents = 50
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        ConcreteProgress(
+            text = stringResource(R.string.amount_trophies, 5, 10),
+            progress = 0.5f,
+            progressInPercents = 50
+        )
+    }
+}
+
+@Composable
+fun ConcreteProgress(
+    text: String,
+    progress: Float,
+    progressInPercents: Int
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(
+            text = text,
+            fontSize = 20.sp
+        )
+        Box() {
+            LinearProgressIndicator(
+                progress = progress,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(25.dp)
+                    .clip(CircleShape),
+                color = ProgressYellow,
+                backgroundColor = LightGray
+            )
+            Text(
+                text = stringResource(R.string.percents, progressInPercents),
+                modifier = Modifier.align(Alignment.CenterEnd).padding(end = 5.dp)
+            )
+        }
+    }
 }
