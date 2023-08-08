a7e02f01c28830ab50de04c38ed95fcf82cbcc15
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2023-08-08 23:23:52 +0300

    Добавлен пиксельный шрифт

--- app/src/main/java/com/mxpj/speedyart/presentation/game/ColorPalette.kt
+++ app/src/main/java/com/mxpj/speedyart/presentation/game/ColorPalette.kt
@@ -3,11 +3,9 @@ package com.mxpj.speedyart.presentation.game
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.VectorPainter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
--- app/src/main/java/com/mxpj/speedyart/presentation/game/StatisticsScreen.kt
+++ app/src/main/java/com/mxpj/speedyart/presentation/game/StatisticsScreen.kt
@@ -1,7 +1,6 @@
 package com.mxpj.speedyart.presentation.game
 
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
@@ -12,13 +11,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Gray
 import androidx.compose.ui.graphics.Color.Companion.LightGray
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,7 +63,8 @@ fun GeneralProgress() {
         Text(
             text = "70%",
             modifier = Modifier.align(Alignment.Center),
-            fontSize = 20.sp
+            fontFamily = FontFamily.getSilverFont(),
+            fontSize = 42.sp
         )
         CircularProgressIndicator(
             progress = 0.8f,
@@ -108,7 +110,8 @@ fun ConcreteProgress(
     Column(modifier = Modifier.fillMaxWidth()) {
         Text(
             text = text,
-            fontSize = 20.sp
+            fontSize = 32.sp,
+            fontFamily = FontFamily.getSilverFont()
         )
         Box() {
             LinearProgressIndicator(
@@ -122,8 +125,18 @@ fun ConcreteProgress(
             )
             Text(
                 text = stringResource(R.string.percents, progressInPercents),
-                modifier = Modifier.align(Alignment.CenterEnd).padding(end = 5.dp)
+                modifier = Modifier
+                    .align(Alignment.CenterEnd)
+                    .padding(end = 5.dp),
+                fontFamily = FontFamily.getSilverFont(),
+                fontSize = 28.sp
             )
         }
     }
+}
+
+fun FontFamily.Companion.getSilverFont(): FontFamily {
+    return FontFamily(arrayListOf<Font>().apply {
+        add(Font(R.font.silver))
+    })
 }
