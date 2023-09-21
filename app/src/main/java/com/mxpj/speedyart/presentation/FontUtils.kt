f2b16ca5a47a922ee2235350818349fbc81fc32e
PuzhaikinMaxim <56976303+PuzhaikinMaxim@users.noreply.github.com>
2023-09-21 21:47:35 +0300

    Добавлен экран настроек

--- app/src/main/java/com/mxpj/speedyart/presentation/FontUtils.kt
+++ app/src/main/java/com/mxpj/speedyart/presentation/FontUtils.kt
@@ -4,6 +4,12 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import com.mxpj.speedyart.R
 
+val FontFamily.Companion.Silver: FontFamily
+    get() = FontFamily(arrayListOf<Font>().apply {
+        add(Font(R.font.silver))
+    })
+
+
 fun FontFamily.Companion.getSilverFont(): FontFamily {
     return FontFamily(arrayListOf<Font>().apply {
         add(Font(R.font.silver))
--- app/src/main/java/com/mxpj/speedyart/presentation/SettingsScreen.kt
+++ app/src/main/java/com/mxpj/speedyart/presentation/SettingsScreen.kt
@@ -1,16 +1,20 @@
 package com.mxpj.speedyart.presentation
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Switch
-import androidx.compose.material.SwitchDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mxpj.speedyart.ui.theme.DarkSky
+import androidx.compose.ui.unit.sp
+import com.mxpj.speedyart.R
 
 @Preview
 @Composable
@@ -20,11 +24,9 @@ fun SettingsScreen() {
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             DarkModeSetting()
-            Spacer(modifier = Modifier.height(10.dp))
-            FontSetting()
-            Spacer(modifier = Modifier.height(10.dp))
+            Spacer(modifier = Modifier.height(20.dp))
             ResetProgressSetting()
         }
     }
@@ -32,30 +34,57 @@ fun SettingsScreen() {
 
 @Composable
 fun DarkModeSetting() {
-    DarkModeButton()
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            stringResource(R.string.dark_mode_setting),
+            fontFamily = FontFamily.Silver,
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        DarkModeButton()
+    }
 }
 
 @Composable
 fun DarkModeButton(initialMode: Boolean = false) {
     var isDarkModeOn by remember { mutableStateOf(initialMode) }
-    Switch(
+    CustomSwitch(
         checked = isDarkModeOn,
-        onCheckedChange = {isDarkModeOn = !isDarkModeOn},
-        colors = SwitchDefaults.colors(
-            checkedTrackColor = DarkSky,
-            checkedTrackAlpha = 1f,
-            checkedThumbColor = Color.Gray
-        ),
-        modifier = Modifier.padding(5.dp)
-    )
-}
-
-@Composable
-fun FontSetting() {
-
+        thumbSize = 25.dp,
+        height = 30.dp,
+        width = 80.dp,
+        strokeWidth = 0.dp
+    ){
+        isDarkModeOn = !isDarkModeOn
+    }
 }
 
 @Composable
 fun ResetProgressSetting() {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            stringResource(R.string.reset_progress_setting),
+            fontFamily = FontFamily.Silver,
+            fontSize = 30.sp
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        ResetProgressButton()
+    }
+}
 
+@Composable
+fun ResetProgressButton() {
+    Button(
+        onClick = {},
+        modifier = Modifier
+            .width(80.dp)
+            .height(40.dp),
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.LightGray)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_restart_progress),
+            contentDescription = ""
+        )
+    }
 }
